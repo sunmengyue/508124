0624057d9bdb78f1e82b6521a175fb9adf1f62f5
sunmengyue <mengyuesun@gmail.com>
2021-12-09 00:24:09 -0500

    complete basic design layout

--- client/src/Login.js
+++ client/src/Login.js
@@ -8,23 +8,49 @@ import {
   Button,
   FormControl,
   TextField,
-  CssBaseline,
-  Paper,
 } from "@material-ui/core";
 import { login } from "./store/utils/thunkCreators";
 import { makeStyles } from "@material-ui/core/styles";
-import BgImg from './assets/bg-img.png'
+import BgImg from './assets/bg-img.png';
+import DialogBubble from './assets/chat.png';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     height: "100vh",
-    width: "100vw",
-    display: "flex"
+    maxWidth: 2000,
+    margin: "auto",
+    display: "flex",
+    overflow: "hidden"
   }, 
-  loginForm: {
-    flex: 8 
- 
-  }
+  banner: {
+    height: "100%",
+    backgroundImage: `linear-gradient(to bottom, rgba(58, 141, 255, 0.7), rgba(58, 141, 255, 0.9)), url(${BgImg})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    textAlign: "center",
+    [theme.breakpoints.down('xs')]: {
+      display: 'none'
+    }
+  },
+  bannerText: {
+    color: "#fff",
+    lineHeight: 1.5
+  },
+
+  rightForm: {
+    height: "100%",
+    paddingLeft:"10%" ,
+    paddingRight: "10%",
+  },
+
+  formWelcome: {
+    fontSize: 30,
+    fontWeight: 600,
+  },
+
+  rightFormBody: {
+    marginTop: 50
+  },
 }));
 
 const Login = (props) => {
@@ -45,44 +71,50 @@ const Login = (props) => {
   }
 
   return (
-    <Grid container justify="center" className={classes.root} component="main">
-      <CssBaseline />
-      <Box>
-        <img src={BgImg} alt="background" height="100%"/>
-      </Box>
-      <Box className={classes.loginForm}>
-        <Grid container item>
-          <Typography>Need to register?</Typography>
-          <Button onClick={() => history.push("/register")}>Register</Button>
-        </Grid>
-        <form onSubmit={handleLogin}>
+    <Grid container className={classes.root} >
+      <Grid item className={classes.banner} sm={5} xl={4}>
+        <Box style={{marginTop: 200}}>
+          <img src={DialogBubble} style={{marginBottom: 30}}/>
+          <Typography variant="h4" className={classes.bannerText}>Converse with anyone with any language</Typography>
+        </Box>
+      </Grid>
+      <Grid item container className={classes.rightForm} sm={7} xl={8}>
+          <Grid container item className={classes.rightFormHeader} justifyContent="flex-end" alignItems="center">
+            <Typography style={{ marginRight: "5%"}}>Don't have an account?</Typography>
+            <Button onClick={() => history.push("/register")} variant="text" color="primary" >Create account</Button>
+          </Grid>
+        <form onSubmit={handleLogin} className={classes.rightFormBody}>
           <Grid>
-            <Grid>
-              <FormControl margin="normal" required>
+            <Typography variant="h3" className={classes.formWelcome} style={{ marginBottom: 50 }}>Welcom back!</Typography>
+            <Grid >
+              <FormControl margin="normal" required >
                 <TextField
                   aria-label="username"
                   label="Username"
                   name="username"
                   type="text"
+                  style={{width: 900}}
                 />
               </FormControl>
             </Grid>
             <FormControl margin="normal" required>
               <TextField
+                fullWidth
                 label="password"
                 aria-label="password"
                 type="password"
                 name="password"
+                style={{width: 900, marginTop: 80}}
               />
             </FormControl>
-            <Grid>
-              <Button type="submit" variant="contained" size="large">
+            <Grid container justifyContent="center" alignItems="center">
+              <Button type="submit" variant="contained" size="large" color="primary" style={{ marginTop: 100, width: 200, height: 70}}>
                 Login
               </Button>
             </Grid>
           </Grid>
         </form>
-      </Box>
+      </Grid>
     </Grid>
   );
 };
